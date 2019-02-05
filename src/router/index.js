a832ae7fe94a2dc7a798c48bde7fd07003d1b736
zhangjiayue <alertyue@163.com>
2019-02-05 22:37:25 +0800

    rights开始
    rights开始

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/login/login.vue'
 import Home from '@/components/pages/home.vue'
+import User from '@/components/pages/user/user.vue'
+import Rights from '@/components/pages/rights/rights.vue'
 
 Vue.use(Router)
 
@@ -9,6 +11,10 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {name: 'login', path: '/login', component: Login},
-    {name: 'home', path: '/home', component: Home}
+    {name: 'home', path: '/home', component: Home, children: [
+      // 挂载子路由
+      { path: '/user', component: User},
+      { path: '/rights', component: Rights}
+    ]},
   ]
 })
